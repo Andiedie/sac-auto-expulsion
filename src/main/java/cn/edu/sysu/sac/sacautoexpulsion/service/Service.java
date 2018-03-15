3c8d988c71e51a8825da1bc6b493c301b5d3c53a
Andie <zchangan@163.com>
2018-03-15 18:20:35 +0800

    0.0.2

--- src/main/java/cn/edu/sysu/sac/sacautoexpulsion/service/Service.java
+++ src/main/java/cn/edu/sysu/sac/sacautoexpulsion/service/Service.java
@@ -5,34 +5,36 @@ import cn.edu.sysu.sac.sacautoexpulsion.entity.MusicProfile;
 import cn.edu.sysu.sac.sacautoexpulsion.entity.PlayerStatus;
 import com.sun.javafx.application.PlatformImpl;
 import javafx.scene.media.Media;
-import javafx.scene.media.MediaException;
 import javafx.scene.media.MediaPlayer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
-import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.Date;
 
 @org.springframework.stereotype.Service
 public class Service {
-    static {
+    @Value("${cn.edu.sysu.sac.sacautoexpulsion.defaultPath}")
+    private void prepare(String path) throws IOException {
+        Resource resource = new ClassPathResource(path);
         PlatformImpl.startup(() -> {});
+        Path mp3 = Files.createTempFile("now-playing", ".mp3");
+        Files.copy(resource.getInputStream(), mp3, StandardCopyOption.REPLACE_EXISTING);
+        media = new Media(mp3.toFile().toURI().toString());
     }
+
     private MediaPlayer mp;
     private PlayerStatus status = new PlayerStatus();
 
-    @Value("${cn.edu.sysu.sac.sacautoexpulsion.defaultPath}")
-    private String defaultPath;
+    private Media media;
 
     public void start(MusicProfile profile) throws BadRequest {
         stop();
-        Media hit = null;
-        try {
-            hit = new Media(new ClassPathResource(defaultPath).getFile().toURI().toString());
-        } catch (Exception e) {
-            throw new BadRequest("找不到指定的文件。");
-        }
-        mp = new MediaPlayer(hit);
+        mp = new MediaPlayer(media);
         mp.setCycleCount(MediaPlayer.INDEFINITE);
         mp.play();
         status.setStartTime(new Date());
